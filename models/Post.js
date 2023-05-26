1055efeb6f41fcd3145d7e835572545dcc148dc2
salwa f <salwa.imleta@gmail.com>
2023-05-26 11:26:23 +0700

    materi tgl 26
    visual studio code

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+    content: {
+        type: String,
+        require: true
+    },
+    user_id: {
+        type: String,
+        require: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    },
+    Modified_date: {
+        type: Date,
+        default: null
+    }
+})
+
+module.exports = mongoose.model('Post', postSchema)
